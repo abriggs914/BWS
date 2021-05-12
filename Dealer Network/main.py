1e10aad2af48fa97fbda31129b66acb48c11e8fb
abriggs1 <abriggs1@unb.ca>
2021-05-12 17:01:00 -0300

    2021-05-12 End of day file push

--- Dealer Network/main.py
+++ Dealer Network/main.py
@@ -1,84 +1,11 @@
-# # import the library
-# import folium
-# # import time
-#
-# # Make an empty map
-# m = folium.Map(location=[20,0], tiles="OpenStreetMap", zoom_start=2)
-#
-# # Show the map
-# m
-# # time.sleep(16)
-#
-# # Import the pandas library
-import pandas as pd
-import csv
-#
-# # Make a data frame with dots to show on the map
-#
-# data
-#
-# # add marker one by one on the map
-# for i in range(0,len(data)):
-#    folium.Marker(
-#       location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
-#       popup=data.iloc[i]['name'],
-#    ).add_to(m)
-#
-# # Show the map again
-# m
-#
-# # Make an empty map
-# n = folium.Map(location=[20,0], tiles="OpenStreetMap", zoom_start=2)
-#
-# # add marker one by one on the map
-# for i in range(0,len(data)):
-#    folium.Marker(
-#       location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
-#       popup=data.iloc[i]['name'],
-#       icon=folium.DivIcon(html=f"""<div style="font-family: courier new; color: blue">{data.iloc[i]['name']}</div>""")
-#    ).add_to(n)
-#
-# # Show the map again
-# n
-# import folium
-# from IPython.display import HTML, display
-# # LDN_COORDINATES = (51.5074, 0.1278)
-# # myMap = folium.Map(location=LDN_COORDINATES, zoom_start=12)
-# # display(myMap)
-#
-# m = folium.Map([43,-100], zoom_start=4)
-#
-# html="""
-#     <h1> This is a big popup</h1><br>
-#     With a few lines of code...
-#     <p>
-#     <code>
-#         from numpy import *<br>
-#         exp(-2*pi)
-#     </code>
-#     </p>
-#     """
-# iframe = folium.element.IFrame(html=html, width=500, height=300)
-# popup = folium.Popup(iframe, max_width=2650)
-#
-# folium.Marker([30,-100], popup=popup).add_to(m)
-#
-# display(m)
-
 import folium
 import webbrowser
-import requests
 
-dealerships = {}
-data = pd.DataFrame({
-    'lon': [-58, 2, 145, 30.32, -4.03, -73.57, 36.82, -38.5],
-    'lat': [-34, 49, -38, 59.93, 5.33, 45.52, -1.29, -12.97],
-    'name': ['Buenos Aires', 'Paris', 'melbourne', 'St Petersbourg', 'Abidjan', 'Montreal', 'Nairobi', 'Salvador'],
-    'value': [10, 12, 40, 70, 23, 43, 100, 43]
-}, dtype=str)
+########################################################################################################################
 
 
 class Map(folium.Map):
+
     def __init__(self, center, zoom_start, **kwargs):
         super().__init__(**kwargs)
         self.center = center
@@ -87,39 +14,60 @@ class Map(folium.Map):
     def showMap(self):
         # Create the map
         # my_map = folium.Map(location=self.center, zoom_start=self.zoom_start)
-        self.location=self.center
-        self.zoom_start= self.zoom_start
+        self.location = self.center
+        self.zoom_start = self.zoom_start
         # Display the map
         self.save("map.html")
         webbrowser.open("map.html")
 
 
+def gen_pop_up_data(d):
+    # dat = "Company:\n\t" + d['COMPANY NAME']
+    # dat += "\nAddr:\n\t" + ", ".join([d["ADDRESS"], d["CITY"], d["PROVINCE"], d["POSTAL CODE"]])
+    dat = '''<pre>Company:<br><br>    {cn}<br><br>Address:<br><br>    {ca}, {cc}, {cp}, {cpc}</pre>'''.format(
+        cn=d["COMPANY NAME"], ca=d["ADDRESS"], cc=d["CITY"], cp=d["PROVINCE"], cpc=d["POSTAL CODE"])
+    iframe = folium.IFrame(dat)
+    popup = folium.Popup(iframe,
+                         min_width=450,
+                         max_width=450)
+    return popup
+
+
+data = {}
+
+with open("out.csv", "r") as f:
+    # f_dicts = csv.DictReader(f, delimiter=";;")
+    f_lines = f.readlines()
+    header = ""
+    for i, f_line in enumerate(f_lines):
+        f_spl = list(map(lambda s: str(s).strip(), f_line.split(";;")))
+        if i > 0:
+            if "NULL" not in f_line:
+                f_dict = dict(zip(header, f_spl))
+                data[i] = f_dict
+                # for k, v in f_dict.items():
+                #     print("k:", k, "v:", v)
+                # data[i] = {str(k).strip(): str(v).strip() for k, v in f_dict.items()}
+        else:
+            header = f_spl
+
 # Define coordinates of where we want to center our map
-coords = [46.427230736465326, -67.70498453648138]
-mp = Map(center=coords, zoom_start=13)
+coords = [46.427230736465326, -67.70498453648138]  # BWS Centreville
+mp = Map(center=coords, zoom_start=1)
 
 # add marker one by one on the map
-for i in range(0, len(data)):
+for i in range(1, len(data)):
+    print("data[i]:", data[i])
     folium.Marker(
-        location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
-        popup=data.iloc[i]['name'],
+        location=[data[i]['LAT'], data[i]['LON']],
+        popup=gen_pop_up_data(data[i]),
     ).add_to(mp)
 mp.showMap()
 
-response = requests.get(
-    'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA')
-
-resp_json_payload = response.json()
-
-print(resp_json_payload)
-print(resp_json_payload['results'])
-# print(resp_json_payload['results'][0]['geometry']['location'])
-
-if False:
-    with open("dealers_info.csv", "r") as f, open("out.csv", "w") as o, open("addresses.txt", "w") as a:
-        f_dicts = csv.DictReader(f)
-        o.write(";;".join(f_dicts.fieldnames))
-        for f_dict in f_dicts:
-            if "NULL" not in f_dict.values():
-                o.write("\n" + ";;".join(list(map(lambda s: str(s).strip(), f_dict.values()))))
-                a.write(", ".join(list(map(lambda s: str(s).strip(), f_dict.values()))) + "\n")
+# response = requests.get(
+#     'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA')
+#
+# resp_json_payload = response.json()
+#
+# print(resp_json_payload)
+# print(resp_json_payload['results'])
---
+++ Dealer Network/scratch.py
@@ -0,0 +1,130 @@
+
+import pandas as pd
+import csv
+import requests
+import folium
+import webbrowser
+
+# # import the library
+# import folium
+# # import time
+#
+# # Make an empty map
+# m = folium.Map(location=[20,0], tiles="OpenStreetMap", zoom_start=2)
+#
+# # Show the map
+# m
+# # time.sleep(16)
+#
+# # Import the pandas library
+
+THIS_VAR_MUST_BE_FALSE_SO_AS_TO_NOT_OVERWRITE_ADDRESS_FILES = False
+#
+# # Make a data frame with dots to show on the map
+#
+# data
+#
+# # add marker one by one on the map
+# for i in range(0,len(data)):
+#    folium.Marker(
+#       location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
+#       popup=data.iloc[i]['name'],
+#    ).add_to(m)
+#
+# # Show the map again
+# m
+#
+# # Make an empty map
+# n = folium.Map(location=[20,0], tiles="OpenStreetMap", zoom_start=2)
+#
+# # add marker one by one on the map
+# for i in range(0,len(data)):
+#    folium.Marker(
+#       location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
+#       popup=data.iloc[i]['name'],
+#       icon=folium.DivIcon(html=f"""<div style="font-family: courier new; color: blue">{data.iloc[i]['name']}</div>""")
+#    ).add_to(n)
+#
+# # Show the map again
+# n
+# import folium
+# from IPython.display import HTML, display
+# # LDN_COORDINATES = (51.5074, 0.1278)
+# # myMap = folium.Map(location=LDN_COORDINATES, zoom_start=12)
+# # display(myMap)
+#
+# m = folium.Map([43,-100], zoom_start=4)
+#
+# html="""
+#     <h1> This is a big popup</h1><br>
+#     With a few lines of code...
+#     <p>
+#     <code>
+#         from numpy import *<br>
+#         exp(-2*pi)
+#     </code>
+#     </p>
+#     """
+# iframe = folium.element.IFrame(html=html, width=500, height=300)
+# popup = folium.Popup(iframe, max_width=2650)
+#
+# folium.Marker([30,-100], popup=popup).add_to(m)
+#
+# display(m)
+
+
+class Map(folium.Map):
+    def __init__(self, center, zoom_start, **kwargs):
+        super().__init__(**kwargs)
+        self.center = center
+        self.zoom_start = zoom_start
+
+    def showMap(self):
+        # Create the map
+        # my_map = folium.Map(location=self.center, zoom_start=self.zoom_start)
+        self.location = self.center
+        self.zoom_start = self.zoom_start
+        # Display the map
+        self.save("map.html")
+        webbrowser.open("map.html")
+
+
+if THIS_VAR_MUST_BE_FALSE_SO_AS_TO_NOT_OVERWRITE_ADDRESS_FILES:
+    dealerships = {}
+    data = pd.DataFrame({
+        'lon': [-58, 2, 145, 30.32, -4.03, -73.57, 36.82, -38.5],
+        'lat': [-34, 49, -38, 59.93, 5.33, 45.52, -1.29, -12.97],
+        'name': ['Buenos Aires', 'Paris', 'melbourne', 'St Petersbourg', 'Abidjan', 'Montreal', 'Nairobi', 'Salvador'],
+        'value': [10, 12, 40, 70, 23, 43, 100, 43]
+    }, dtype=str)
+
+    # Define coordinates of where we want to center our map
+    coords = [46.427230736465326, -67.70498453648138]
+    mp = Map(center=coords, zoom_start=13)
+
+    # add marker one by one on the map
+    for i in range(0, len(data)):
+        folium.Marker(
+            location=[data.iloc[i]['lat'], data.iloc[i]['lon']],
+            popup=data.iloc[i]['name'],
+        ).add_to(mp)
+    mp.showMap()
+
+    response = requests.get(
+        'https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA')
+
+    resp_json_payload = response.json()
+
+    print(resp_json_payload)
+    print(resp_json_payload['results'])
+    # print(resp_json_payload['results'][0]['geometry']['location'])
+
+if THIS_VAR_MUST_BE_FALSE_SO_AS_TO_NOT_OVERWRITE_ADDRESS_FILES:
+    with open("dealers_info.csv", "r") as f, open("out.txt", "w") as o, open("addresses.txt", "w") as a:
+        f_dicts = csv.DictReader(f)
+        o.write(";;".join(f_dicts.fieldnames))
+        for f_dict in f_dicts:
+            if "NULL" not in f_dict.values():
+                o.write("\n" + ";;".join(list(map(lambda s: str(s).strip(), f_dict.values()))))
+                a.write(", ".join(list(map(lambda s: str(s).strip(), f_dict.values()))) + "\n")
+
