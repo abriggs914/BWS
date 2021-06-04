40b313a7eb916e07fff4a28b34fe5a3191d6f5d6
abriggs1 <abriggs1@unb.ca>
2021-06-04 16:30:23 -0300

    2021-06-04 End of day file push

--- SQL Scripts/winch part numbers.sql
+++ SQL Scripts/winch part numbers.sql
@@ -1,6 +1,30 @@
 USE BWSdb
 GO
 
+/*
 SELECT * FROM [Options]
 	WHERE [Description] LIKE '%5820%'
 	ORDER BY [Model No], [Draw/Part#]
+*/
+
+SELECT * FROM [Order Hours] AS A WITH (NOLOCK)
+	WHERE A.[Quote#] IN
+		(SELECT [Quote#] FROM [Orders] WITH (NOLOCK)
+			WHERE [Model No] LIKE '%42et%')
+		AND [Step 1] != 28
+		AND [Step 2] != 28
+	ORDER BY [Quote#]
+	
+SELECT * FROM [Order Hours] AS A WITH (NOLOCK)
+	WHERE A.[Quote#] IN
+		(SELECT [Quote#] FROM [Orders] WITH (NOLOCK)
+			WHERE [Model No] LIKE '%48et%')
+		AND [Step 1] != 28
+		AND [Step 2] != 28
+	ORDER BY [Quote#]
+
+	
+
+	--WHERE []
+
+SELECT * FROM [Orders] WITH (NOLOCK)
