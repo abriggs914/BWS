b26e8de0262a0eca9ced4a9bf16d3de9a1b0388b
abriggs1 <abriggs1@unb.ca>
2021-06-23 16:54:55 -0300

    2021-06-23 End of day file push

---
+++ SQL Scripts/Slot Program Identification/stored_procedure.sql
@@ -0,0 +1,46 @@
+USE BWSdb
+GO
+
+IF EXISTS (SELECT 1 FROM SYS.procedures where name ='GetSlotReport')
+BEGIN
+DROP PROCEDURE [GetSlotReport]
+END
+GO
+ 
+CREATE PROCEDURE [GetSlotReport]
+	(@StartDate Date)
+AS
+BEGIN
+	-- Monthly slot counts for each dealer
+	WITH SlotTypes AS (
+		SELECT
+			ROW_NUMBER() OVER (
+				PARTITION BY [Dealer], [Slot Types]
+				ORDER BY [Dealer] ASC
+			) AS row_num, *
+		FROM
+			[Production Slots]
+	)
+	
+	SELECT 
+		[Dealers].[COMPANY NAME], [Slot Types], DateName(mm, [Slot Date]) AS [Month], COUNT(*) AS [# Slots]
+	FROM
+		SlotTypes WITH (NOLOCK)
+	INNER JOIN
+		[Dealers]
+	ON
+		[Dealers].[ID] = [SlotTypes].[Dealer]
+	WHERE 
+		[Slot Date] >= @StartDate
+	GROUP BY
+		[Dealers].[COMPANY NAME], [Slot Types], [Slot Date]
+	ORDER BY
+		[Dealers].[COMPANY NAME], [Slot Types], [Slot Date]
+END
+;
+GO
+
+EXEC
+	[GetSlotReport]
+		@StartDate = '2021-06-23'
+;
