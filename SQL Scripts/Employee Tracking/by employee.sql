8b8dffeee25a4c5abb6a04dff54b8add7588749c
abriggs1 <abriggs1@unb.ca>
2021-07-22 17:06:00 -0300

    2021-07-22 End of day file push

--- SQL Scripts/Employee Tracking/by employee.sql
+++ SQL Scripts/Employee Tracking/by employee.sql
@@ -33,6 +33,13 @@ GO
 
 -- SELECT * FROM SysproCompanyA.dbo.WipLabJnl with (nolock)
 
+DECLARE @PRINT BIT = 1;
+IF @PRINT = 1 BEGIN
+	SELECT * FROM [Employees] WITH (NOLOCK) -- 526 records AS OF 2021-07-22
+	SELECT * FROM [SysproCompanyA].[dbo].[WipLabJnl] WITH (NOLOCK) ORDER BY [EntryDate] DESC-- 895 023 records AS OF 2021-07-22
+	SELECT * FROM [dtProductionSchedule] WITH (NOLOCK) -- 5035 records AS OF 2021-07-22
+END
+
 SELECT
 	[2nd Name], [1st Name], [Machine], *
 FROM
@@ -50,3 +57,115 @@ WHERE
 	AND [Prod Date 1] > '2021-01-01' 
 ORDER BY
 	[Prod Date 1] DESC
+
+	
+
+
+DECLARE @SD AS DATETIME;
+DECLARE @ED AS DATETIME;
+DECLARE @MACHINES AS TABLE ([MachineID] INT);
+INSERT INTO @MACHINES VALUES (1);
+SET @SD = '2021-01-01'
+SET @ED = '2021-08-01'
+
+
+SELECT
+	[2nd Name],
+	[1st Name],
+	[Machine], 
+	*
+FROM (
+	SELECT
+		*
+	FROM
+		[SysproCompanyA].[dbo].[WipLabJnl] WITH (NOLOCK)
+	WHERE
+		[EntryDate] BETWEEN @SD AND @ED
+) AS [WipLab]
+INNER JOIN
+	[dtProductionSchedule]
+ON 
+	CAST([dtProductionSchedule].[WO#] AS VARCHAR(20)) = [WipLab].[Job]
+INNER JOIN
+	[Employees]
+ON 
+	(SUBSTRING([Employee], 4, 6)) = CAST([Employees].[Emp#] AS VARCHAR(20))
+ORDER BY
+	[EntryDate], [Employees].[2nd Name], [Employees].[1st Name]
+;
+
+
+SELECT * FROM [Defects_Location]
+
+
+SELECT
+	*
+FROM
+	[Defects]
+INNER JOIN
+	[Defects_Location]
+ON
+	[Defects].[LocationID] = [Defects_Location].[LocationID#]
+WHERE
+	((([Defects].[Input Date]) Between @SD And @ED))
+;
+
+SELECT * FROM [uniPoint_Live].[dbo].[PT_Equip]
+WHERE [Category] in (
+	('T1'),
+	('T2'),
+	('T3'),
+	('T4'),
+	('T5'),
+	('T6'),
+	('T7'),
+	('T8'),
+	('T9'),
+	('T10'),
+	('T11'),
+	('BEAMS'),
+	('G1'),
+	('G2'),
+	('PONY FRAME'),
+	('SUB ASSY.')
+)
+
+
+-----------------------------------------------------------------------------------------------------------------------
+
+DECLARE @SD AS DATETIME;
+DECLARE @ED AS DATETIME;
+DECLARE @MACHINES AS TABLE ([MachineID] INT);
+INSERT INTO @MACHINES VALUES (1);
+SET @SD = '2021-01-01'
+SET @ED = '2021-08-01'
+
+
+SELECT
+	[2nd Name],
+	[1st Name],
+	[Machine], 
+	*
+FROM (
+	SELECT
+		*
+	FROM
+		[SysproCompanyA].[dbo].[WipLabJnl] WITH (NOLOCK)
+	WHERE
+		[EntryDate] BETWEEN @SD AND @ED
+) AS [WipLab]
+INNER JOIN
+	[dtProductionSchedule]
+ON 
+	CAST([dtProductionSchedule].[WO#] AS VARCHAR(20)) = [WipLab].[Job]
+INNER JOIN
+	[Employees]
+ON 
+	(SUBSTRING([Employee], 4, 6)) = CAST([Employees].[Emp#] AS VARCHAR(20))
+INNER  JOIN
+	[uniPoint_Live].[dbo].[PT_Equip]
+ON
+	[Machine] LIKE CAST([EquipID] AS VARCHAR(20))
+ORDER BY
+	[EntryDate], [Employees].[2nd Name], [Employees].[1st Name]
+;
---
+++ SQL Scripts/Employee Tracking/employees.sql
@@ -0,0 +1,32 @@
+USE BWSdb
+GO
+
+
+SELECT * FROM [WO]
+SELECT * FROM [v_Access/SysproWORef]
+SELECT * FROM [Order Hours]
+SELECT * FROM [Hours Worked]
+SELECT * FROM [Hours Required]
+
+SELECT
+	[Emp#],
+	([2nd Name] + ', ' + [1st Name]) AS [Name],
+	[Status]
+FROM
+	[Employees]
+WHERE
+	[Emp#] IS NOT NULL
+	AND [1st Name] IS NOT NULL
+	AND [2nd Name] IS NOT NULL
+ORDER BY
+	[Name]
+
+SELECT * FROM [Status]
+
+SELECT * FROM [Defects]
+
+
+SELECT Employees.[Emp#], (Employees.[2nd Name] + ', ' + Employees.[1st Name]) AS [Name], Employees.	[Status]
+FROM Employees
+WHERE Employees.[Emp#] IS NOT NULL 	AND Employees.[1st Name] IS NOT NULL 	AND Employees.[2nd Name] IS NOT NULL 
+ORDER BY [Name]
