cbbb6131b4f800ad1ab78bd524a24255cc0b466a
abriggs1 <abriggs1@unb.ca>
2021-10-04 16:58:34 -0300

    2021-10-04 End of day file push

--- SQL Scripts/Top Level Parts/top_level.sql
+++ SQL Scripts/Top Level Parts/top_level.sql
@@ -1,14 +1,45 @@
+--USE SysproCompanyA
+--GO
+
+--SELECT * FROM [WipMaster] WITH (NOLOCK) ORDER BY [Job] DESC;
+--SELECT * FROM [MrpJobMaster] WITH (NOLOCK) ORDER BY [Job] DESC;
+--SELECT * FROM [WipJobAllLab] WITH (NOLOCK) ORDER BY [Job] DESC;
+--SELECT * FROM [WipLabJnl] WITH (NOLOCK) ORDER BY [Job] DESC;
+--SELECT * FROM [WipJobAllMat];
+
+
+--USE BWSdb
+--GO
+
+--SELECT * FROM [Production]
+
+-----------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------
 USE SysproCompanyA
 GO
 
-SELECT * FROM [WipMaster] WITH (NOLOCK) ORDER BY [Job] DESC;
-SELECT * FROM [MrpJobMaster] WITH (NOLOCK) ORDER BY [Job] DESC;
-SELECT * FROM [WipJobAllLab] WITH (NOLOCK) ORDER BY [Job] DESC;
-SELECT * FROM [WipLabJnl] WITH (NOLOCK) ORDER BY [Job] DESC;
-SELECT * FROM [WipJobAllMat];
+DECLARE @WO AS VARCHAR(8);
+SET @WO = '10014747';
 
-
-USE BWSdb
-GO
-
-SELECT * FROM [Production]
+SELECT
+	[JobStartDate] AS [WipJobAllMat.JobStartDate],
+	[WipJobAllMat].[Job] AS [WipJobAllMat.Job],
+	[WipMaster].[Job] AS [WipMaster.Job],
+	[WipJobAllMat].[StockCode] AS [WipMaster.StockCode],
+	[WipJobAllMat].[StockDescription] AS [WipMaster.StockDescription],
+	[WipMaster].[Complete] AS [WipMaster.Complete],
+	[WipJobAllMat].[QtyIssued] AS [WipJobAllMat.QtyIssued],
+	[WipMaster].[QtyToMake] AS [WipMaster.QtyToMake],
+	*
+FROM
+	[WipJobAllMat] WITH (NOLOCK)
+INNER JOIN
+	[WipMaster] WITH (NOLOCK)
+ON
+	[WipJobAllMat].[StockCode] = [WipMaster].[StockCode] and [WipJobAllMat].[Warehouse] = [WipMaster].[Warehouse]
+WHERE
+	[WipJobAllMat].[Job] = @WO
+ORDER BY
+	[WipMaster].[JobStartDate] DESC
+;
