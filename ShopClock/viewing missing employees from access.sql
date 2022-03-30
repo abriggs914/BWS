88e3462250ff54d25132fccf94bf1fbd89a588dd
abriggs1 <abriggs1@unb.ca>
2022-03-30 16:47:51 -0300

    2022-03-30 End of day file push

--- ShopClock/viewing missing employees from access.sql
+++ ShopClock/viewing missing employees from access.sql
@@ -4,6 +4,9 @@ GO
 -- Viewing employees who make transactions, but who hav not been pulled into the access rounding program yet.
 -- 2022-03-16
 
+BEGIN TRAN;
+
+-- Missing list
 SELECT
 	*
 FROM
@@ -14,3 +17,39 @@ ON
 	[ClkTransaction].[EmployeeNumber] = [ClkShiftEmpAssign].Emp#
 WHERE
 	[ClkShiftEmpAssign].[Emp#] IS NULL
+;
+SELECT * FROM [ClkShiftEmpAssign] ORDER BY [Emp#] DESC;
+
+INSERT INTO
+	[ClkShiftEmpAssign]
+([Emp#], [ShiftID])
+SELECT
+	[EmployeeNumber],
+	1
+FROM
+	[ClkTransaction]
+LEFT JOIN
+	[ClkShiftEmpAssign]
+ON
+	[ClkTransaction].[EmployeeNumber] = [ClkShiftEmpAssign].Emp#
+WHERE
+	[ClkShiftEmpAssign].[Emp#] IS NULL
+GROUP BY
+	[EmployeeNumber]
+
+-- Missing list - should be empty now
+SELECT
+	*
+FROM
+	[ClkTransaction]
+LEFT JOIN
+	[ClkShiftEmpAssign]
+ON
+	[ClkTransaction].[EmployeeNumber] = [ClkShiftEmpAssign].Emp#
+WHERE
+	[ClkShiftEmpAssign].[Emp#] IS NULL
+;
+SELECT * FROM [ClkShiftEmpAssign] ORDER BY [Emp#] DESC;
+
+ROLLBACK;
+COMMIT;
