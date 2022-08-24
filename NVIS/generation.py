29f910293a899883c91d7a7e30efc74f56b5d1ad
Avery <abriggs1@unb.ca>
2022-08-24 09:33:32 -0300

    2022-08-24 End of day file push

--- NVIS/generation.py
+++ NVIS/generation.py
@@ -1,3 +1,5 @@
+import pandas
+
 from pyodbc_connection import connect
 import tkinter
 
@@ -110,12 +112,23 @@ class NVIS:
         self.production_year = production_year
         self.sequential_start = sequential_start
         self._serial = None
+        self._server_serial = None
+        self._model_no = None
+        self._wo = None
+        self._length = None
+        self._axles = None
+        self._type_of_trailer = None
+        self._type_of_body = None
+        self._model_year = None
+        self._units_to_date = None
+        self._check_digit = None
 
     def calculate(self):
         self.details["orders"] = connect(f"SELECT * FROM [Orders] WHERE [Quote#] = {self.details['quote']}")
         self.details["snc_year"] = connect(f"SELECT * FROM [SNC Year] WHERE [Year] = {self.details['year']}")
+        model_name = self.details['orders']['Model No'].tolist()[0]
         self.details["snc_type"] = connect(
-            f"SELECT * FROM [SN Type] WHERE [Model No] = '{self.details['orders']['Model No'].tolist()[0]}'")
+            f"SELECT * FROM [SN Type] WHERE [Model No] = '{model_name}'")
 
         # print(f"details['orders']\n:{self.details['orders']}")
         # print(f"details['snc_year']\n:{self.details['snc_year']}")
@@ -157,13 +170,12 @@ class NVIS:
                         ;
                         """
 
-            if self.details["sequential"] is None:
-                sequential = connect(sql_str)["X"].tolist()[0]
-                sequential = (-1 if sequential is None else sequential) + 1
-            else:
-                sequential = int(self.details["sequential"]) + 1
+            units_to_date = connect(sql_str)["X"].tolist()[0]
+            units_to_date = (-1 if units_to_date is None else units_to_date) + 1
+            if self.details["sequential"] is not None:
+                units_to_date = int(self.details["sequential"]) + 1
 
-            sequential = ("000000" + str(sequential))[-6:]
+            sequential = ("000000" + str(units_to_date))[-6:]
 
             serial[0] = "2"  # Assigned by CVMA
             serial[1] = "X"  # Assigned by CVMA
@@ -182,13 +194,36 @@ class NVIS:
 
             serial[8] = calc_check_digit()  # Check digit
 
+            self.model_no = model_name
+            self.wo = self.details["orders"]["WO#"].tolist()[0]
+            self.length = serial[6]
+            self.axles = serial[7]
+            self.type_of_trailer = serial[4]
+            self.type_of_body = serial[5]
+            self.model_year = serial[9]
+            self.check_digit = serial[8]
+            self.units_to_date = units_to_date
+
             # serial = list(map(str, serial))
             serial = [str(letter) if letter is not None else "_" for letter in serial]
 
             self.serial = ''.join(serial)
+            ss = connect(f"EXEC [sp_SerialNumberCalc] @quote={self.details['quote']}, @year={self.details['year']}")
+            if ss is not None and (isinstance(ss, pandas.DataFrame) and ss.size > 0):
+                cols = ss.columns
+                self.server_serial = ss[cols[0]].tolist()[0]
 
         else:
             print(f"Error, no records returned for quote: {self.details['quote']} for year: {self.details['year']}")
+
+    def get_server_serial(self):
+        return self._server_serial
+
+    def set_server_serial(self, server_serial_in):
+        self._server_serial = server_serial_in
+
+    def del_server_serial(self):
+        del self._server_serial
 
     def get_serial(self):
         if self._serial is None:
@@ -201,7 +236,98 @@ class NVIS:
     def del_serial(self):
         del self._serial
 
+    def get_model_no(self):
+        return self._model_no
+
+    def set_model_no(self, model_no_in):
+        self._model_no = model_no_in
+
+    def del_model_no(self):
+        del self._model_no
+
+    def get_wo(self):
+        return self._wo
+
+    def set_wo(self, wo_in):
+        self._wo = wo_in
+
+    def del_wo(self):
+        del self._wo
+
+    def get_length(self):
+        return self._length
+
+    def set_length(self, length_in):
+        self._length = length_in
+
+    def del_length(self):
+        del self._length
+
+    def get_axles(self):
+        return self._axles
+
+    def set_axles(self, axles_in):
+        self._axles = axles_in
+
+    def del_axles(self):
+        del self._axles
+
+    def get_type_of_trailer(self):
+        return self._type_of_trailer
+
+    def set_type_of_trailer(self, type_of_trailer_in):
+        self._type_of_trailer = type_of_trailer_in
+
+    def del_type_of_trailer(self):
+        del self._type_of_trailer
+
+    def get_type_of_body(self):
+        return self._type_of_body
+
+    def set_type_of_body(self, type_of_body_in):
+        self._type_of_body = type_of_body_in
+
+    def del_type_of_body(self):
+        del self._type_of_body
+
+    def get_model_year(self):
+        return self._model_year
+
+    def set_model_year(self, model_year_in):
+        self._model_year = model_year_in
+
+    def del_model_year(self):
+        del self._model_year
+
+    def get_units_to_date(self):
+        return self._units_to_date
+
+    def set_units_to_date(self, units_to_date_in):
+        self._units_to_date = units_to_date_in
+
+    def del_units_to_date(self):
+        del self._units_to_date
+
+    def get_check_digit(self):
+        return self._check_digit
+
+    def set_check_digit(self, check_digit_in):
+        self._check_digit = check_digit_in
+
+    def del_check_digit(self):
+        del self._check_digit
+
     def __repr__(self):
         return str(self.serial)
 
+    server_serial = property(get_server_serial, set_server_serial, del_server_serial)
     serial = property(get_serial, set_serial, del_serial)
+    model_no = property(get_model_no, set_model_no, del_model_no)
+    wo = property(get_wo, set_wo, del_wo)
+    length = property(get_length, set_length, del_length)
+    axles = property(get_axles, set_axles, del_axles)
+    type_of_trailer = property(get_type_of_trailer, set_type_of_trailer, del_type_of_trailer)
+    type_of_body = property(get_type_of_body, set_type_of_body, del_type_of_body)
+    model_year = property(get_model_year, set_model_year, del_model_year)
+    units_to_date = property(get_units_to_date, set_units_to_date, del_units_to_date)
+    check_digit = property(get_check_digit, set_check_digit, del_check_digit)
--- NVIS/main.py
+++ NVIS/main.py
@@ -4,19 +4,33 @@ import tkinter
 from generation import NVIS
 from tkinter import ttk
 
+from utility import clamp
+
+
 class NVISGenerator(tkinter.Tk):
 
     def __init__(self):
         super().__init__()
 
-        self.geometry(f"{400}x{350}")
+        self.geometry(f"{400}x{400}")
         self.title("BWS NVIS Generator")
+
+        today_year = datetime.datetime.now().year
+        self.valid_combo_dates = list(range(today_year - 5, today_year + 5))
+
+        self.style = ttk.Style(self)
+        self.style.theme_use("default")
+        self.style.map("Entry")
+        self.style.map("Button")
+        self.style.map("Label")
+        self.style.map("Combobox")
 
         self.tv_label_title = tkinter.StringVar(self, value="Enter a Quote# and select a Model Year to generate a new Serial Number.")
         self.tv_label_quote = tkinter.StringVar(self, value="Quote#:")
         self.tv_label_model_year = tkinter.StringVar(self, value="Model Year:")
         self.tv_label_status = tkinter.StringVar(self, value="Status:")
-        self.tv_btn_generate = tkinter.StringVar(self, value="generate")
+        self.tv_btn_generate = tkinter.StringVar(self, value="generate serial")
+        self.tv_btn_clear = tkinter.StringVar(self, value="clear fields")
         self.tv_label_sequential = tkinter.StringVar(self, value="Sequential:")
         self.tv_quote = tkinter.StringVar(self)
         self.tv_model_year = tkinter.StringVar(self)
@@ -25,13 +39,14 @@ class NVISGenerator(tkinter.Tk):
 
         self.label_title = tkinter.Label(self, textvariable=self.tv_label_title)
         self.label_quote = tkinter.Label(self, textvariable=self.tv_label_quote)
-        self.entry_quote_number = tkinter.Entry(self, textvariable=self.tv_quote)
+        self.entry_quote_number = tkinter.Entry(self, textvariable=self.tv_quote, width=25)
         self.label_model_year = tkinter.Label(self, textvariable=self.tv_label_model_year)
-        self.combo_model_year = ttk.Combobox(self, textvariable=self.tv_model_year)
-        self.entry_status = tkinter.Entry(self, textvariable=self.tv_status, state="readonly")
+        self.combo_model_year = ttk.Combobox(self, textvariable=self.tv_model_year, width=25, show=10)
+        self.entry_status = tkinter.Entry(self, textvariable=self.tv_status, state="readonly", width=50, justify="center")
         self.label_sequential = tkinter.Label(self, textvariable=self.tv_label_sequential)
-        self.entry_sequential = tkinter.Entry(self, textvariable=self.tv_sequential)
-        self.button_generate = tkinter.Button(self, textvariable=self.tv_btn_generate, command=self.generate_NVIS)
+        self.entry_sequential = tkinter.Entry(self, textvariable=self.tv_sequential, width=25)
+        self.button_generate = tkinter.Button(self, textvariable=self.tv_btn_generate, command=self.generate_NVIS, width=24)
+        self.button_clear = tkinter.Button(self, textvariable=self.tv_btn_clear, command=self.clear_fields, width=24)
         self.label_status_code = tkinter.Label(self, textvariable=self.tv_label_status)
 
         self.tv_label_dat_model_no = tkinter.StringVar(self, value="Model No:")
@@ -41,7 +56,9 @@ class NVISGenerator(tkinter.Tk):
         self.tv_label_dat_trailer_type = tkinter.StringVar(self, value="Type of Trailer:")
         self.tv_label_dat_body_type = tkinter.StringVar(self, value="Type of Body:")
         self.tv_label_dat_model_year = tkinter.StringVar(self, value="Model Year:")
+        self.tv_label_dat_units_date = tkinter.StringVar(self, value="Units to Date:")
         self.tv_label_dat_check_digit = tkinter.StringVar(self, value="Check Digit:")
+        self.tv_label_dat_server_serial = tkinter.StringVar(self, value="Server Serial:")
         self.tv_dat_model_no = tkinter.StringVar(self)
         self.tv_dat_wo = tkinter.StringVar(self)
         self.tv_dat_length = tkinter.StringVar(self)
@@ -49,7 +66,9 @@ class NVISGenerator(tkinter.Tk):
         self.tv_dat_trailer_type = tkinter.StringVar(self)
         self.tv_dat_body_type = tkinter.StringVar(self)
         self.tv_dat_model_year = tkinter.StringVar(self)
+        self.tv_dat_units_date = tkinter.StringVar(self)
         self.tv_dat_check_digit = tkinter.StringVar(self)
+        self.tv_dat_server_serial = tkinter.StringVar(self)
         self.label_dat_model_no = tkinter.Label(self, textvariable=self.tv_label_dat_model_no)
         self.label_dat_wo = tkinter.Label(self, textvariable=self.tv_label_dat_wo)
         self.label_dat_length = tkinter.Label(self, textvariable=self.tv_label_dat_length)
@@ -57,7 +76,9 @@ class NVISGenerator(tkinter.Tk):
         self.label_dat_trailer_type = tkinter.Label(self, textvariable=self.tv_label_dat_trailer_type)
         self.label_dat_body_type = tkinter.Label(self, textvariable=self.tv_label_dat_body_type)
         self.label_dat_model_year = tkinter.Label(self, textvariable=self.tv_label_dat_model_year)
+        self.label_dat_units_date = tkinter.Label(self, textvariable=self.tv_label_dat_units_date)
         self.label_dat_check_digit = tkinter.Label(self, textvariable=self.tv_label_dat_check_digit)
+        self.label_dat_server_serial = tkinter.Label(self, textvariable=self.tv_label_dat_server_serial)
         self.entry_dat_model_no = tkinter.Entry(self, textvariable=self.tv_dat_model_no, state="readonly")
         self.entry_dat_wo = tkinter.Entry(self, textvariable=self.tv_dat_wo, state="readonly")
         self.entry_dat_length = tkinter.Entry(self, textvariable=self.tv_dat_length, state="readonly")
@@ -65,42 +86,46 @@ class NVISGenerator(tkinter.Tk):
         self.entry_dat_trailer_type = tkinter.Entry(self, textvariable=self.tv_dat_trailer_type, state="readonly")
         self.entry_dat_body_type = tkinter.Entry(self, textvariable=self.tv_dat_body_type, state="readonly")
         self.entry_dat_model_year = tkinter.Entry(self, textvariable=self.tv_dat_model_year, state="readonly")
+        self.entry_dat_units_date = tkinter.Entry(self, textvariable=self.tv_dat_units_date, state="readonly")
         self.entry_dat_check_digit = tkinter.Entry(self, textvariable=self.tv_dat_check_digit, state="readonly")
+        self.entry_dat_server_serial = tkinter.Entry(self, textvariable=self.tv_dat_server_serial, state="readonly")
 
-        today_year = datetime.datetime.now().year
-        self.combo_model_year["values"] = list(range(today_year - 5, today_year + 6))
+        self.combo_model_year["values"] = self.valid_combo_dates
 
         self.grid()
-        self.label_title.grid(row=1, column=1, columnspan=4)
-        self.label_quote.grid(row=2, column=1, columnspan=2)
-        self.entry_quote_number.grid(row=1, column=3, columnspan=2)
-        self.label_quote.grid(row=3, column=1, columnspan=2)
-        self.entry_quote_number.grid(row=3, column=3, columnspan=2)
-        self.label_model_year.grid(row=4, column=1, columnspan=2)
-        self.combo_model_year.grid(row=4, column=3, columnspan=2)
-        self.label_sequential.grid(row=5, column=1, columnspan=2)
-        self.entry_sequential.grid(row=5, column=3, columnspan=2)
-        self.button_generate.grid(row=6, column=2, columnspan=2)
-        self.entry_status.grid(row=7, column=1, columnspan=4)
+        self.label_title.grid(row=1, column=1, columnspan=4, padx=0, pady=10)
+        self.label_quote.grid(row=2, column=1, columnspan=2, padx=0, pady=0)
+        self.entry_quote_number.grid(row=1, column=3, columnspan=2, padx=0, pady=0)
+        self.label_quote.grid(row=3, column=1, columnspan=2, padx=0, pady=0)
+        self.entry_quote_number.grid(row=3, column=3, columnspan=2, padx=0, pady=0)
+        self.label_model_year.grid(row=4, column=1, columnspan=2, padx=0, pady=0)
+        self.combo_model_year.grid(row=4, column=3, columnspan=2, padx=0, pady=0)
+        self.label_sequential.grid(row=5, column=1, columnspan=2, padx=0, pady=0)
+        self.entry_sequential.grid(row=5, column=3, columnspan=2, padx=0, pady=0)
+        self.button_generate.grid(row=6, column=1, columnspan=2, padx=5, pady=5)
+        self.button_clear.grid(row=6, column=3, columnspan=2, padx=5, pady=5)
+        self.entry_status.grid(row=7, column=1, columnspan=4, padx=10, pady=10)
 
-
-        self.label_dat_model_no.grid(row=10, column=1, columnspan=2)
-        self.label_dat_wo.grid(row=11, column=1, columnspan=2)
-        self.label_dat_length.grid(row=12, column=1, columnspan=2)
-        self.label_dat_axles.grid(row=13, column=1, columnspan=2)
-        self.label_dat_trailer_type.grid(row=14, column=1, columnspan=2)
-        self.label_dat_body_type.grid(row=15, column=1, columnspan=2)
-        self.label_dat_model_year.grid(row=16, column=1, columnspan=2)
-        self.label_dat_check_digit.grid(row=17, column=1, columnspan=2)
-
-        self.entry_dat_model_no.grid(row=10, column=3, columnspan=2)
-        self.entry_dat_wo.grid(row=11, column=3, columnspan=2)
-        self.entry_dat_length.grid(row=12, column=3, columnspan=2)
-        self.entry_dat_axles.grid(row=13, column=3, columnspan=2)
-        self.entry_dat_trailer_type.grid(row=14, column=3, columnspan=2)
-        self.entry_dat_body_type.grid(row=15, column=3, columnspan=2)
-        self.entry_dat_model_year.grid(row=16, column=3, columnspan=2)
-        self.entry_dat_check_digit.grid(row=17, column=3, columnspan=2)
+        self.label_dat_model_no.grid(row=10, column=1, columnspan=2, padx=0, pady=0)
+        self.entry_dat_model_no.grid(row=10, column=3, columnspan=2, padx=0, pady=0)
+        self.label_dat_wo.grid(row=11, column=1, columnspan=2, padx=0, pady=0)
+        self.entry_dat_wo.grid(row=11, column=3, columnspan=2, padx=0, pady=0)
+        self.label_dat_length.grid(row=12, column=1, columnspan=2, padx=0, pady=0)
+        self.entry_dat_length.grid(row=12, column=3, columnspan=2, padx=0, pady=0)
+        self.label_dat_axles.grid(row=13, column=1, columnspan=2, padx=0, pady=0)
+        self.entry_dat_axles.grid(row=13, column=3, columnspan=2, padx=0, pady=0)
+        self.label_dat_trailer_type.grid(row=14, column=1, columnspan=2, padx=0, pady=0)
+        self.entry_dat_trailer_type.grid(row=14, column=3, columnspan=2, padx=0, pady=0)
+        self.label_dat_body_type.grid(row=15, column=1, columnspan=2, padx=0, pady=0)
+        self.entry_dat_body_type.grid(row=15, column=3, columnspan=2, padx=0, pady=0)
+        self.label_dat_model_year.grid(row=16, column=1, columnspan=2, padx=0, pady=0)
+        self.entry_dat_model_year.grid(row=16, column=3, columnspan=2, padx=0, pady=0)
+        self.label_dat_units_date.grid(row=17, column=1, columnspan=2, padx=0, pady=0)
+        self.entry_dat_units_date.grid(row=17, column=3, columnspan=2, padx=0, pady=0)
+        self.label_dat_check_digit.grid(row=18, column=1, columnspan=2, padx=0, pady=0)
+        self.entry_dat_check_digit.grid(row=18, column=3, columnspan=2, padx=0, pady=0)
+        self.label_dat_server_serial.grid(row=19, column=1, columnspan=2, padx=0, pady=0)
+        self.entry_dat_server_serial.grid(row=19, column=3, columnspan=2, padx=0, pady=0)
 
     def error_status(self, message="Error, please fix input params."):
         self.tv_status.set(message)
@@ -113,12 +138,69 @@ class NVISGenerator(tkinter.Tk):
             self.tv_status.set(status_in)
             self.entry_status.config(foreground=foreground)
 
+    def set_serial(self, serial_in):
+        assert isinstance(serial_in, NVIS), f"Error, serial_in needs to be a NVIS object: {serial_in}, {type(serial_in)}"
+        self.set_status(status_in=str(serial_in))
+        self.tv_dat_model_no.set(serial_in.model_no)
+        self.tv_dat_wo.set(serial_in.wo)
+        self.tv_dat_length.set(serial_in.length)
+        self.tv_dat_axles.set(serial_in.axles)
+        self.tv_dat_trailer_type.set(serial_in.type_of_trailer)
+        self.tv_dat_body_type.set(serial_in.type_of_body)
+        self.tv_dat_model_year.set(serial_in.model_year)
+        self.tv_dat_units_date.set(serial_in.units_to_date)
+        self.tv_dat_check_digit.set(serial_in.check_digit)
+        self.tv_dat_server_serial.set(serial_in.server_serial)
+
+    def clear_sn_detail(self):
+        """Clear the Serial Number detail text fields."""
+        self.tv_dat_model_no.set("")
+        self.tv_dat_wo.set("")
+        self.tv_dat_length.set("")
+        self.tv_dat_axles.set("")
+        self.tv_dat_trailer_type.set("")
+        self.tv_dat_body_type.set("")
+        self.tv_dat_model_year.set("")
+        self.tv_dat_units_date.set("")
+        self.tv_dat_check_digit.set("")
+        self.tv_dat_server_serial.set("")
+
+    def clear_fields(self):
+        """Clear top 3 textfields. Quote#, Model Year, and Sequential Start #"""
+        self.tv_quote.set("")
+        self.tv_model_year.set("")
+        self.tv_sequential.set("")
+
+        self.clear_sn_detail()
+
+    def validate_combo(self):
+        value = self.tv_model_year.get()
+        if value not in self.valid_combo_dates:
+            if isinstance(value, int) or isinstance(value, float):
+                value = clamp(self.valid_combo_dates[0], value, self.valid_combo_dates[-1])
+            else:
+                value = datetime.datetime.now().year
+        else:
+            value = datetime.datetime.now().year
+        print(f"Validating value: {value}")
+        self.tv_model_year.set(str(value))
+
     def generate_NVIS(self):
+        """Using Quote# and Model Year, generate a NVIS or serial number by BWS standards. 2022-08-23"""
         quote = self.tv_quote.get()
         model_year = self.tv_model_year.get()
         sequential = self.tv_sequential.get()
         sequential = sequential if sequential is not None and len(sequential) > 0 else None
-        self.tv_status.set(str(NVIS(quote, model_year, sequential_start=sequential)))
+        # self.tv_status.set(str(NVIS(quote, model_year, sequential_start=sequential)))
+        # self.set_status(status_in=str(NVIS(quote, model_year, sequential_start=sequential)))
+        try:
+            self.set_serial(NVIS(quote, model_year, sequential_start=sequential))
+        except IndexError as ie:
+            self.set_status(str(ie), is_error=True)
+        except KeyError as ke:
+            self.set_status(str(ke), is_error=True)
+        except ValueError as ve:
+            self.set_status(str(ve), is_error=True)
 
 if __name__ == "__main__":
     # print(NVIS(26454, 2022))
