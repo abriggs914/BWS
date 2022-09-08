90bd2cb0d3c765e928f0b94219d2447e372a0981
Avery <abriggs1@unb.ca>
2022-09-08 07:44:43 -0300

    2022-09-08 End of day file push

--- Production Schedule Stargate/scheduler_app.py
+++ Production Schedule Stargate/scheduler_app.py
@@ -83,21 +83,48 @@ class App(tkinter.Tk):
         # Order and production data pertaining to Stargate units
         self.df_production = connect("""
 
+
+
+-- Final Select Query for all STG Prod dates
+
 SELECT
-	*
+	[dtProductionSchedule].*
+	, [dtProductionScheduleV2].*
+	, [OrdersV2].*
 FROM
-	[dtProductionSchedule]
-LEFT JOIN
 	[BWSdb].[dbo].[OrdersV2]
+LEFT JOIN 
+	[dtProductionSchedule]
 ON
 	[dtProductionSchedule].[SGQuote] = [OrdersV2].[SGQuote]
+LEFT JOIN 
+	[dtProductionScheduleV2]
+ON
+	[dtProductionScheduleV2].[SGQuote] = [OrdersV2].[SGQuote]
 WHERE
-	ISNULL([dtProductionSchedule].[Prod Date 1], [dtProductionSchedule].[Prod Date 2]) IS NOT NULL
+	[dtProductionScheduleV2].[JobFinishDate] IS NOT NULL
 ORDER BY
-	ISNULL([dtProductionSchedule].[Prod Date 1], [dtProductionSchedule].[Prod Date 2])
+	[dtProductionScheduleV2].[JobFinishDate]
 ;
 
         """, database="Stargatedb", uid="SGeu1", pwd="Pupplies-Hagard->Rio0")
+#         self.df_production = connect("""
+#
+# SELECT
+# 	*
+# FROM
+# 	[dtProductionSchedule]
+# LEFT JOIN
+# 	[BWSdb].[dbo].[OrdersV2]
+# ON
+# 	[dtProductionSchedule].[SGQuote] = [OrdersV2].[SGQuote]
+# WHERE
+# 	ISNULL([dtProductionSchedule].[Prod Date 1], [dtProductionSchedule].[Prod Date 2]) IS NOT NULL
+# ORDER BY
+# 	ISNULL([dtProductionSchedule].[Prod Date 1], [dtProductionSchedule].[Prod Date 2])
+# ;
+#
+#         """, database="Stargatedb", uid="SGeu1", pwd="Pupplies-Hagard->Rio0")
 
         self.df_work_days = connect("""SELECT * FROM [v_CalendarWorkDays] ORDER BY [CalendarDate] DESC""", database="SysproCompanyS", uid="SCSRS", pwd="")
 
