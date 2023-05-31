a1a03fecea8e717bf95b9cc94d95db3dd34e6b4a
Avery Briggs <abriggs1@unb.ca>
2023-05-31 18:30:27 -0300

    2023-05-31 End of day file push

---
+++ NVIS/Stargate/Populating SN Type/selects.sql
@@ -0,0 +1,32 @@
+-- MSN STG Selects
+
+SELECT
+	*
+FROM
+	[MSN STG Trailer Type]
+;
+SELECT
+	*
+FROM
+	[MSN STG Body Types]
+;
+
+SELECT
+	*
+FROM
+	[MSN STG Lengths]
+;
+
+SELECT
+	*
+FROM
+	[SN Type V2]
+ORDER BY
+	[Model No]
+;
+
+SELECT
+	*
+FROM
+	[SNC Year]
+;
