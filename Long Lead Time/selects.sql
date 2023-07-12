529657450b9a810de4bde22d6371766ec05e579d
Avery Briggs <abriggs1@unb.ca>
2023-07-12 18:01:54 -0300

    2023-07-12 End of day file push

---
+++ Long Lead Time/selects.sql
@@ -0,0 +1,26 @@
+USE Stargatedb
+GO
+
+
+SELECT
+	*
+FROM
+	[LongLeadQuoteDetails] AS [A]
+INNER JOIN
+	[LongLeadQuoteMaster] AS [B]
+ON
+	[A].[LongLeadQuoteID#] = [B].[LongLeadQuoteID#]
+INNER JOIN
+	[LongLeadQuotePartClass] AS [C]
+ON
+	[A].[PartClassID] = [C].[PartClassID]
+
+
+SELECT
+	InvMaster.StockCode,
+	InvMaster.Description,
+	InvMaster.[LongDesc]
+FROM
+	[SysproCompanyS].[dbo].[InvMaster]
+ORDER BY
+	InvMaster.StockCode; 
