072af33d6eb3df9583c226013d07a3a672e73297
Avery Briggs <abriggs1@unb.ca>
2023-07-31 17:52:53 -0300

    2023-07-31 End of day file push

---
+++ SQL Scripts/missing drawing multimedia stargate.sql
@@ -0,0 +1,11 @@
+USE SysproCompanyS
+GO
+
+SELECT * FROM [InvMaster]
+SELECT * FROM [InvDocument]
+SELECT * FROM [InvInspect]
+SELECT * FROM [InvControl]
+SELECT * FROM [InvWarehouse]
+SELECT * FROM [InvNarration]
+SELECT * FROM [InvWhAmendJnl]
+SELECT * FROM [WipInspect]
