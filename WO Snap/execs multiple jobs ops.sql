467e060d2440145856c189d3af72679a7848f95b
Avery Briggs <abriggs1@unb.ca>
2024-04-12 19:32:27 -0300

    2024-04-12 End of day file push

--- WO Snap/execs multiple jobs ops.sql
+++ WO Snap/execs multiple jobs ops.sql
@@ -1,2 +1,7 @@
 EXEC [SysproCompanyA].[dbo].[sp_WSS_Multiple_Jobs_OPS_BWS] @WOS='10014545', @OPS='4;5'
 EXEC [SysproCompanyS].[dbo].[sp_WSS_Multiple_Jobs_OPS_STG] @WOS='10001445', @OPS='4;5'
+
+
+
+
+EXEC [SysproCompanyA].[dbo].[sp_WSS_Multiple_Jobs_OPS_BWS] @WOS='10016844', @OPS='2;3;4;5;6;7;8;12'
