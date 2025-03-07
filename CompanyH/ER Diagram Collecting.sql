64c3ac6f7c3c1c449d3582cddb2e5ee3818f8ad9
abriggs914 <abriggs914@gmail.com>
2025-03-07 19:15:53 -0400

    2025-03-07 End of day file push

---
+++ CompanyH/ER Diagram Collecting.sql
@@ -0,0 +1,60 @@
+
+-- Company
+[Dept]
+[Employees]
+
+-- Unit Data
+[SalesPeople]
+[Products]
+[Options]
+[Standards]
+[Standards_FactoryLines]
+[Standards_SpecLines]
+
+-- Unit sold
+[Orders]
+[Dealers]
+[DealerBranch]
+[DealerSalesPeople]
+[Customers]
+[Order Standards]
+[Order Standards_FactoryLines]
+[Order Standards_SpecLines]
+[Option Standards]
+[Option Standards_FactoryLines]
+[Option Standards_SpecLines]
+[Custom Work]
+[Custom Work_FactoryLines]
+[Custom Work_SpecLines]
+
+-- Engineers
+[Design]
+
+-- SYSPRO / BOM entry
+-- Syspro tables
+[Order Hours]
+9
+-- Defects (Planning Phase 1 Only)
+[Print Defects]
+
+-- Schedule Work
+[Production]
+[dtProductionSchedule]
+
+-- Defects (Planning Phase 2 Only)
+[Receiving Defects]
+
+-- Work
+-- Syspro Tables ShopClock
+
+-- Unit Done 
+[BOL]
+[Carrier]
+
+-- Customer Feedback
+[Unipoint] surveys
+
+-- Future Problem
+[Warranty Claims]0014
+[Restorations]
+
