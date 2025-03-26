668bf66eb482c7f09dceba8aa7a0f1cf67835f4c
abriggs914 <abriggs914@gmail.com>
2025-03-26 20:44:51 -0300

    2025-03-26 End of day file push

--- StreamlitApp/Final Costing/Final Costing/testing_20250325.py
+++ StreamlitApp/Final Costing/Final Costing/testing_20250325.py
@@ -143,22 +143,24 @@ def top_n_with_other_bin3(
 
 	return pd.concat(result_frames, ignore_index=True)
 
+
 def top_n_with_other_bin4(
-	df: pd.DataFrame,
+	data: pd.DataFrame,
 	column: Union[str, List[str]],
 	top_n: int = 3,
 	other_label: str = "other",
 	group_by: Union[None, str, int, List[Union[str, int]]] = None,
 	dropna: bool = False,
 	split_value: bool = True,
-	split_group: bool = True
+	split_group: bool = True,
+	replace_value_col: bool = True
 ) -> pd.DataFrame:
 	"""
 	Returns a DataFrame with the top N most frequent values in one or more columns and groups the rest into an 'other' bin.
 	Optionally groups the result by one or more other columns before aggregation.
 
 	Args:
-		df (pd.DataFrame): The input DataFrame.
+		data (pd.DataFrame): The input DataFrame.
 		column (str or list): Column name(s) to analyze.
 		top_n (int): Number of top values to keep.
 		other_label (str): Label for the aggregated 'other' bin.
@@ -177,19 +179,59 @@ def top_n_with_other_bin4(
 	elif group_by is None:
 		group_by = []
 
-	group_by_cols = [df.columns[i] if isinstance(i, int) else i for i in group_by]
-	result_frames = []
+	group_by_cols = [data.columns[i] if isinstance(i, int) else i for i in group_by]
+	result_records = []
 
-	grouped = df.groupby(group_by_cols, dropna=dropna) if group_by_cols else [(None, df)]
+	grouped = data.groupby(group_by_cols, dropna=dropna) if group_by_cols else [(None, data)]
 
 	delim: str = " _||_ "
+	value_name: str = "Value" if (replace_value_col or (len(column) > 1)) else column[0]
 
-	data_keys = [
-		"Group",
-		"Value",
-		"Count",
-		"Proportion"
-	]
+	# data_keys = [
+	# 	"Group",
+	# 	value_name,
+	# 	"Count",
+	# 	"Proportion"
+	# ]
+
+	# for group_vals, sub_df in grouped:
+	# 	group_keys = sub_df[column].astype(str).agg(delim.join, axis=1)
+	# 	counts = group_keys.value_counts(dropna=dropna)
+	# 	proportions = group_keys.value_counts(dropna=dropna, normalize=True)
+	# 	top_values = counts.head(top_n).index
+	#
+	# 	if isinstance(group_vals, tuple):
+	# 		group_label = delim.join(str(v) for v in group_vals)
+	# 	elif group_vals is not None:
+	# 		group_label = str(group_vals)
+	# 	else:
+	# 		group_label = "All"
+	#
+	# 	group_df = pd.DataFrame(dict(zip(data_keys, [
+	# 		group_label,
+	# 		top_values,
+	# 		counts.loc[top_values].values,
+	# 		proportions.loc[top_values].values
+	# 	])))
+	#
+	# 	if len(counts) > top_n:
+	# 		other_count = counts.loc[~counts.index.isin(top_values)].sum()
+	# 		other_prop = proportions.loc[~proportions.index.isin(top_values)].sum()
+	# 		other_val = f"{other_label}_{group_label}" if group_by else other_label
+	# 		data = dict(zip(data_keys, [
+	# 			group_label,
+	# 			other_val,
+	# 			other_count,
+	# 			other_prop
+	# 		]))
+	# 		group_df = pd.concat([
+	# 			group_df,
+	# 			pd.DataFrame([data])
+	# 		], ignore_index=True)
+	#
+	# 	result_frames.append(group_df)
+	#
+	# final: pd.DataFrame = pd.concat(result_frames, ignore_index=True)
 
 	for group_vals, sub_df in grouped:
 		group_keys = sub_df[column].astype(str).agg(delim.join, axis=1)
@@ -204,62 +246,57 @@ def top_n_with_other_bin4(
 		else:
 			group_label = "All"
 
-		group_df = pd.DataFrame(dict(zip(data_keys, [
-			group_label,
-			top_values,
-			counts.loc[top_values].values,
-			proportions.loc[top_values].values
-		])))
+		for val in top_values:
+			result_records.append({
+				"Group": group_label,
+				value_name: val,
+				"Count": counts[val],
+				"Proportion": proportions[val]
+			})
 
 		if len(counts) > top_n:
 			other_count = counts.loc[~counts.index.isin(top_values)].sum()
 			other_prop = proportions.loc[~proportions.index.isin(top_values)].sum()
 			other_val = f"{other_label}_{group_label}" if group_by else other_label
-			data = dict(zip(data_keys, [
-				group_label,
-				other_val,
-				other_count,
-				other_prop
-			]))
-			group_df = pd.concat([
-				group_df,
-				pd.DataFrame([data])
-			], ignore_index=True)
+			result_records.append({
+				"Group": group_label,
+				value_name: other_val,
+				"Count": other_count,
+				"Proportion": other_prop
+			})
 
-		result_frames.append(group_df)
+	final = pd.DataFrame(result_records)
 
-	final: pd.DataFrame = pd.concat(result_frames, ignore_index=True)
-
-	print(f"A")
-	print(final)
+	# print(f"A")
+	# print(final)
 
 	if split_value and len(column) > 1:
-		value_split = final["Value"].str.split(delim, expand=True, regex=False)
+		value_split = final[value_name].str.split(delim, expand=True, regex=False)
 		value_split.columns = column  # data_keys[:1] + column + data_keys[1:]
 		data_keys = value_split.columns
-		final = final.drop(columns="Value").join(value_split)
+		final = value_split.join(final.drop(columns=value_name))
 
-	print(f"B")
-	print(final)
+	# print(f"B")
+	# print(final)
 
 	if split_group and len(group_by_cols) > 1:
-		test_a = final["Group"].str.split(delim, regex=False)
-		print(f"test_a")
-		print(test_a)
+		# test_a = final["Group"].str.split(delim, regex=False)
+		# print(f"test_a")
+		# print(test_a)
 		group_split = final["Group"].str.split(delim, expand=True, regex=False)
-		print(f"test_b")
-		print(group_split)
-		# print(f"test_c")
-		# print(group_split.loc[~group_split[2].isna()])
-		print(f"group_split.columns")
-		print(f"{group_split.columns}")
-		print(f"group_split.group_by_cols")
-		print(f"{group_by_cols}")
+		# print(f"test_b")
+		# print(group_split)
+		# # print(f"test_c")
+		# # print(group_split.loc[~group_split[2].isna()])
+		# print(f"group_split.columns")
+		# print(f"{group_split.columns}")
+		# print(f"group_split.group_by_cols")
+		# print(f"{group_by_cols}")
 		group_split.columns = group_by_cols  #  + data_keys[1:]
-		final = final.drop(columns="Group").join(group_split)
+		final = group_split.join(final.drop(columns="Group"))
 
-	print(f"C")
-	print(final)
+	# print(f"C")
+	# print(final)
 
 	return final
 
@@ -376,13 +413,30 @@ if selectbox_model:
 			))
 
 
-display_df(top_n_with_other_bin4(
-	df,
-	column=[
-		clbl_sortG
-	],
-	group_by=[
-		clbl_modelNo,
-		clbl_group
-	]
-))
+display_df(
+	top_n_with_other_bin4(
+		data=df,
+		column=clbl_sortG,
+		group_by=[
+			clbl_modelNo,
+			clbl_group
+		]
+	),
+"DF__4"
+)
+
+display_df(
+	top_n_with_other_bin4(
+		data=df,
+		column=[
+			clbl_sortSe
+		],
+		group_by=[
+			clbl_modelNo,
+			clbl_group,
+			clbl_section
+		],
+		replace_value_col=False
+	),
+"DF__5"
+)
