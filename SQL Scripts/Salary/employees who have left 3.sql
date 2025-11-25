d2110f085a37f6316f1fdce911320878e1075962
abriggs914 <abriggs914@gmail.com>
2025-11-25 16:26:42 -0400

    2025-11-25 End of day file push

--- SQL Scripts/Salary/employees who have left 3.sql
+++ SQL Scripts/Salary/employees who have left 3.sql
@@ -3,13 +3,14 @@
 SELECT
 	*
 FROM
-	[ClkTransaction] [C]
+	[SysproCompanyA].[dbo].[ClkTransaction] [C]
 WHERE
 	(
 	LOWER([EmployeeName]) LIKE LOWER('%Hath%')
 	OR LOWER([EmployeeName]) LIKE LOWER('%luis p%')
 	OR LOWER([EmployeeName]) LIKE LOWER('%sean hard%')
-	OR LOWER([EmployeeName]) LIKE LOWER('%Mason%'))
+	OR LOWER([EmployeeName]) LIKE LOWER('%Mason%')
+	OR LOWER([EmployeeName]) LIKE LOWER('%damien%'))
 ORDER BY
 	[LoggedOn] DESC
 
@@ -24,6 +25,7 @@ WHERE
 	LOWER([1st Name]) LIKE LOWER('%Hath%')
 	OR LOWER([1st Name]) LIKE LOWER('%luis p%')
 	OR LOWER([1st Name]) LIKE LOWER('%sean hard%')
-	OR LOWER([1st Name]) LIKE LOWER('%Mason%'))
+	OR LOWER([1st Name]) LIKE LOWER('%Mason%')
+	OR LOWER([1st Name]) LIKE LOWER('%damien%'))
 ORDER BY
 	[2nd Name] DESC
